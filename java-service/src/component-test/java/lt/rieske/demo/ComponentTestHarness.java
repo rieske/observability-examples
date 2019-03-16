5bb985a1087b01f0c9386955d0c51f7e1da1f195
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2019-03-16 10:30:10 +0200

    Exposed prometheus metrics endpoint

--- java-service/src/component-test/java/lt/rieske/demo/ComponentTestHarness.java
+++ java-service/src/component-test/java/lt/rieske/demo/ComponentTestHarness.java
@@ -15,19 +15,32 @@ public abstract class ComponentTestHarness {
 
     private static final String SERVICE_CONTAINER = "service_1";
     private static final int SERVICE_PORT = 8080;
+    private static final int ADMIN_PORT = 8081;
 
     @ClassRule
     public static DockerComposeContainer environment =
             new DockerComposeContainer(new File("docker-compose.yml"))
                     .withLogConsumer(SERVICE_CONTAINER, new Slf4jLogConsumer(log).withPrefix(SERVICE_CONTAINER))
                     .withExposedService(SERVICE_CONTAINER, SERVICE_PORT,
+                            Wait.forListeningPort())
+                    .withExposedService(SERVICE_CONTAINER, ADMIN_PORT,
                             Wait.forHttp("/actuator/health").forStatusCode(200));
 
     @Before
     public void setUpEnvironment() {
-        RestAssured.baseURI = String.format("http://%s:%d",
+        RestAssured.baseURI = serviceUrl();
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    protected String serviceUrl() {
+        return String.format("http://%s:%d",
                 environment.getServiceHost(SERVICE_CONTAINER, SERVICE_PORT),
                 environment.getServicePort(SERVICE_CONTAINER, SERVICE_PORT));
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    protected String adminUrl() {
+        return String.format("http://%s:%d",
+                environment.getServiceHost(SERVICE_CONTAINER, ADMIN_PORT),
+                environment.getServicePort(SERVICE_CONTAINER, ADMIN_PORT));
     }
 }
--- java-service/src/component-test/java/lt/rieske/demo/SmokeTest.java
+++ java-service/src/component-test/java/lt/rieske/demo/SmokeTest.java
@@ -2,6 +2,7 @@ package lt.rieske.demo;
 
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 
 public class SmokeTest extends ComponentTestHarness {
@@ -9,8 +10,22 @@ public class SmokeTest extends ComponentTestHarness {
     @Test
     public void serviceStarts() {
         // @formatter:off
-        when()
+        given()
+            .baseUri(adminUrl())
+        .when()
             .get("/actuator/health")
+        .then()
+            .statusCode(200);
+        // @formatter:on
+    }
+
+    @Test
+    public void prometheusMetricsEndpointIsExposed() {
+        // @formatter:off
+        given()
+            .baseUri(adminUrl())
+        .when()
+            .get("/actuator/prometheus")
         .then()
             .statusCode(200);
         // @formatter:on
